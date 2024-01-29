efbb8936eb776f72a2d05c28d89c0edf3e07bef9
Abu Arslan <evta.newredmi@gmail.com>
2024-01-29 15:16:48 +0700

    challenge 4/366

---
+++ 4-smash.js
@@ -0,0 +1,11 @@
+// WPU Coding Challenge 2024
+// 4/366
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d
+
+// function smash(words) {
+//   return words.join(" ");
+// }
+
+const smash = (words) => words.join(" ");
+
+console.log(smash(["hello", "world", "this", "is", "great"]));

b07fa6b17d1daa3a1d9ffe527929b545efb9cb47
Abu Arslan <evta.newredmi@gmail.com>
2024-02-12 20:11:25 +0700

    challenge 14/366

---
+++ 14-getGrade.js
@@ -0,0 +1,10 @@
+// WPU Coding Challenge 2024
+// 13/366
+// https://www.codewars.com/kata/55cbd4ba903825f7970000f5
+
+function getGrade(s1, s2, s3) {
+  const score = (s1 + s2 + s3)/3;
+  if()
+}
+
+console.log(70,70,100);

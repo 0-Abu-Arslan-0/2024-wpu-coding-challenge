700905c2f2b45ddba572f7be68de4fc6e8930558
Abu Arslan <evta.newredmi@gmail.com>
2024-02-13 16:42:18 +0700

    challenge 14/366

--- 14-getGrade.js
+++ 14-getGrade.js
@@ -3,8 +3,19 @@
 // https://www.codewars.com/kata/55cbd4ba903825f7970000f5
 
 function getGrade(s1, s2, s3) {
-  const score = (s1 + s2 + s3)/3;
-  if()
+  let avg = (s1 + s2 + s3) / 3;
+ switch (true) {
+   case avg >= 90:
+     return 'A';
+   case avg >= 80:
+     return 'B';
+   case avg >= 70:
+     return 'C';
+   case avg >= 60:
+     return 'D';
+   default:
+     return 'F';
+ }
 }
 
-console.log(70,70,100);
+console.log(getGrade(95, 90, 93));
